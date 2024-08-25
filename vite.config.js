2fa3bf54ec1a0be438b3d63e68ab8b6a484fd5a2
bsrypt <bsry.nitipoom@gmail.com>
2024-08-25 20:07:06 +0700

    feat(ci): add auto ci

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/wad-project-1/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
